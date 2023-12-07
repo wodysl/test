c8b0dc15a216a4bad8d121176229b8935019a632
MAPOALABA <nayana4848@naver.com>
2023-12-07 15:27:51 +0900

    js 애니메이션 오류 수정

--- wodysl_react_test/src/App.js
+++ wodysl_react_test/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // 아이콘 임포트
-import React from "react";
+import React, { Component, useRef, useEffect, useState} from "react";
+
+// mongoose.connect('mongodb+srv://honghwarang:45fkd62dus@cluster0.sti7z1m.mongodb.net/?retryWrites=true&w=majority',{
+//     useNewUrlParser : true, useUnifiedTopology : true
+// }).then(()=>console.log("MongoDB Connect"))
+//     .catch(err=>console.log(err))
+
+// mongoose.set('strictQuery', true);
+
+//Router
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
 import Sign from './react/Sign';
 import Gpt from './react/Gpt';
 
-
 function App() {
-
   return (
     <div className="App">
     <BrowserRouter>
